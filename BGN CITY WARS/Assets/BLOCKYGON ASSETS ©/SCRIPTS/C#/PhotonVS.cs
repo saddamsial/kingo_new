368bccbf07b4073a195701bba520a6eb06846a9c
superior <superior.ppro@gmail.com>
2022-07-19 09:15:56 -0400

    ADDED MULTIPLAYER

---
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/PhotonVS.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
+
+
+
+public class PhotonVS : MonoBehaviourPunCallbacks
+{
+   public override void OnConnectedToMaster()
+    {
+        CustomEvent.Trigger(gameObject, "OnConnectedToMaster");
+    }
+    public override void OnJoinedRoom()
+    {
+        CustomEvent.Trigger(gameObject, "OnJoinedRoom");
+    }
+}
