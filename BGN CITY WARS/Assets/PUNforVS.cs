0db650cbc6332acc6771432b893f8366a5390ffa
kingo-debug <82778809+kingo-debug@users.noreply.github.com>
2022-07-20 11:45:05 -0400

    Update PUNforVS.cs

--- BGN CITY WARS/Assets/PUNforVS.cs
+++ BGN CITY WARS/Assets/PUNforVS.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.VisualScripting;
+using Photon.Pun;
+using Photon;
 public class PUNforVS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject triggertarget;
+    
+   private void OnPhotonSerializedView(PhotonStream stream,PhotonMessageInfo info)
 
-    // Update is called once per frame
-    void Update()
     {
+
+
         
+
+        
+        CustomEvent.Trigger(triggertarget, OnPhotonSerializedView,stream,info,);
+
+    
     }
 }
