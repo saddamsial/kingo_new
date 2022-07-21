d8081dda3036a4903ba1edf9ed4104b2958601bb
kingo-debug <82778809+kingo-debug@users.noreply.github.com>
2022-07-21 13:37:35 -0400

    save

---
+++ BGN CITY WARS/Assets/RPCCALLS.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using Photon;
+using Photon.Pun;
+
+public class RPCCALLS : MonoBehaviour
+{
+
+
+    public bool IsAIMING;
+
+
+     
+
+
+
+    // RPC FOR OTHERS VARIABLES
+    private void Start()
+    {
+        GameObject player = this.gameObject;
+
+   
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Update()
+    {
+
+
+       
+       
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
