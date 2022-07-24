6c60ede0d3c26a4d6d95148d638f11a201a4bb3d
superior <superior.ppro@gmail.com>
2022-07-24 10:11:02 -0400

    Update ThirdPersonCharacter.cs

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/BARROWED ASSETS/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/BARROWED ASSETS/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
     //  [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(CapsuleCollider))]
-    [RequireComponent(typeof(Animator))]
+   // [RequireComponent(typeof(CapsuleCollider))]
+ //   [RequireComponent(typeof(Animator))]
     public class ThirdPersonCharacter : MonoBehaviour
     {
         [SerializeField] float m_MovingTurnSpeed = 360;
