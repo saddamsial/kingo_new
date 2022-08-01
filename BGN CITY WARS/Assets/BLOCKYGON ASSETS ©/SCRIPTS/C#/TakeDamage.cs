f938f00236baba6e373cd26371913cd0eb8401ff
superior <superior.ppro@gmail.com>
2022-07-31 20:00:48 -0400

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/TakeDamage.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/TakeDamage.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon;
+using Photon.Pun;
 
 public class TakeDamage : MonoBehaviour
 {
     public float HP = 1f;
     public float Shield = 1f;
+    private PhotonView pv;
+    private void Start()
+    {
+        pv = this.GetComponent<PhotonView>();
 
-       
+    }
 
 
 
 
 
-        public void Takedamage(float Damage)
+    public void Takedamage(float Damage)
 
     {//sf
 
-        if (Shield <= 0f)
+        if (Shield <= 0f & pv.IsMine)
         {
             HP = HP - Damage;
         }
