c464d2e4c00923b4bd8c1f2cef50a751f7cbb14c
kingo-debug <82778809+kingo-debug@users.noreply.github.com>
2022-08-03 13:19:06 -0400

    po

--- BGN CITY WARS/Assets/Samples/Cinemachine/2.7.9/Cinemachine Example Scenes/Scenes/Anywhere Door/PlayerMovement.cs
+++ BGN CITY WARS/Assets/Samples/Cinemachine/2.7.9/Cinemachine Example Scenes/Scenes/Anywhere Door/PlayerMovement.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon;
+using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
 public float movementSpeed = 10f;
 public float lookatspeed = 5f;
-   private photonView Pv;
+public PhotonView PV;
 
 
 
-    private void Start()
+    public void Start()
     {
-        Pv = GetComponent<photonView>();
+        PV = GetComponent<PhotonView>();
 
     }
 
@@ -28,25 +29,55 @@ public float lookatspeed = 5f;
 
 
 
-    void Update () {
- 
-        if (Input.GetKey ("w")) {
-            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
-        }   else if (Input.GetKey ("s")) {
-            transform.position -= transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
+    void Update () 
+    {
+        if (PV.IsMine)
+        {
+            Controller();
         }
- 
-        if (Input.GetKey ("a") && !Input.GetKey ("d")) {
-                transform.position += transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-            } else if (Input.GetKey ("d") && !Input.GetKey ("a")) {
-                transform.position -= transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-            }
+     
+        }
+
+
+    void Controller()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
 
         //mouse look at
         float horizontal = Input.GetAxis("Mouse X") * lookatspeed;
-		float vertical = Input.GetAxis("Mouse Y") * lookatspeed;
+        float vertical = Input.GetAxis("Mouse Y") * lookatspeed;
 
-		transform.Rotate(0f, horizontal, 0f, Space.World);
-		//transform.Rotate(-vertical, 0f, 0f, Space.Self);
-        }
+        transform.Rotate(0f, horizontal, 0f, Space.World);
+        //transform.Rotate(-vertical, 0f, 0f, Space.Self);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
