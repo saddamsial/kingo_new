41d5bc32610fccd8b8d9646ff2917aa63c5b647f
superior <superior.ppro@gmail.com>
2022-08-15 00:17:18 -0400

    save

---
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/CarSpawner.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon;
+
+public class CarSpawner : MonoBehaviour
+
+{//SC
+
+//Spawnvars
+public Transform VehiclePos;
+public GameObject Vehicle;
+public Transform Player;
+
+
+//SpawnableVars
+public Vector3 DirectionCheck;
+public float MaxDistanceCheck;
+
+public float BlockRadius;
+
+
+
+[SerializeField]
+private bool Blocked;
+
+//MessageVars
+public GameObject MessageError;
+
+
+
+
+public LayerMask layerMask;
+
+
+//set Player
+void Start()
+{
+    Player = this.transform;
+}
+
+
+
+
+ void Update() 
+ 
+ {
+   
+MessageError.SetActive(Blocked);
+
+    CheckSpawnable();
+     
+    SpawnCar();
+
+    
+
+
+ }
+
+
+
+
+
+void CheckSpawnable()
+{
+
+RaycastHit hit;
+    
+    if (Physics.SphereCast(Player.position,BlockRadius,DirectionCheck,out hit,layerMask))
+    {
+     Blocked= true;
+
+    }
+
+     else Blocked= false;
+
+}
+
+
+
+
+
+
+private void OnDrawGizmos()
+{
+    Gizmos.DrawWireSphere(Player.position,BlockRadius);
+}
+
+
+
+
+
+
+
+
+
+
+
+void SpawnCar()
+
+{
+
+if (Blocked != true && Input.GetKeyDown(KeyCode.E))
+{
+
+PhotonNetwork.Instantiate(Vehicle.name,VehiclePos.position,Quaternion.identity);
+
+
+
+  
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//EC
