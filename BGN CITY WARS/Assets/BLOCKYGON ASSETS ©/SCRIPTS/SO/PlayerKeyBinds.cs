ee2e1ccaa5d73d69a7e99beba3bea705f85f4731
superior <superior.ppro@gmail.com>
2022-08-18 02:11:46 -0400

    savcwe

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/SO/PlayerKeyBinds.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/SO/PlayerKeyBinds.cs
@@ -8,20 +8,19 @@ public class PlayerKeyBinds : ScriptableObject
 
 {
    
-public KeyCode firekey;
-public KeyCode aimkey;  
-public KeyCode jumpkey;
-public KeyCode rollkey;
-public KeyCode reloadkey;
-public KeyCode sprintkey;
-public KeyCode crouchkey;
-public KeyCode emotekey;
-public KeyCode weapon0;
-public KeyCode weapon1;
-public KeyCode weapon2;
-public KeyCode weapon3;
-public KeyCode mapwindow;
-
-
+public string firekey;
+public string aimkey;  
+public string jumpkey;
+public string rollkey = ("Q");
+public string reloadkey;
+public string sprintkey = ("left Shift");
+public string crouchkey;
+public string emotekey;
+public string weapon0;
+public string weapon1;
+public string weapon2;
+public string weapon3;
+public string mapwindow;
+public string carenter = ("F");
 }
 
