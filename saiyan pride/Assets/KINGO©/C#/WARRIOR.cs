0574e2d4b6fcbc476995eb860625cf9091e9b36f
superior <superior.ppro@gmail.com>
2022-09-02 01:44:28 -0400

    save

---
+++ saiyan pride/Assets/KINGO©/C#/WARRIOR.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SETTINGS", menuName = "saiyan pride/WARRIOR", order = 0)]
+public class WARRIOR : ScriptableObject
+ {
+public Sprite UIframe;
+public string Name;
+public float DefaultSpeed;
+public float HP;
+public float Energy;
+public GameObject Aura;
+public GameObject FullPowerAura;
+public GameObject Energy1;
+public GameObject Energy2;
+public GameObject Energy3;
+public GameObject EnergyS;
+// sfx
+public AudioClip [] Entrances;
+
+public AudioClip [] Wins;
+
+public AudioClip [] attacks;
+
+public AudioClip []Energycalls;
+public AudioClip []Charge;
+public AudioClip []FullPower;
+
+
+
+
+
+
+
+
+
+
+ }
+
+
