a167579c655c5809d1f9bf1197f239b4302ac71b
superior <superior.ppro@gmail.com>
2022-09-01 20:17:13 -0400

    SAVE

---
+++ saiyan pride/Assets/KINGO©/C#/SETTINGS.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SETTINGS", menuName = "saiyan pride/SETTINGS", order = 0)]
+public class SETTINGS : ScriptableObject
+ {
+    
+public float SFX;
+public float BGM;
+public bool EasyMode;
+public bool TrainingMode;
+public float UIAlphaTransparency;
+public bool muteBGM;
+public bool muteSFX;
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
---
+++ saiyan pride/Assets/KINGO©/C#/SPZData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SETTINGS", menuName = "saiyan pride/DATA", order = 0)]
+public class SPZData : ScriptableObject
+ {
+    
+public int Level;
+public int ZPoints;
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
