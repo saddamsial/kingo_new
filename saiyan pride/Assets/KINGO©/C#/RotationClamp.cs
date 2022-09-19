f6a3da7ceaae0734f9bd581427e5d5d08e7b4916
superior <superior.ppro@gmail.com>
2022-09-19 12:31:55 -0400

    SAVE

--- saiyan pride/Assets/KINGO©/C#/RotationClamp.cs
+++ saiyan pride/Assets/KINGO©/C#/RotationClamp.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class RotationClamp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+     public float minAngle = -20;
+     public float maxAngle = 40;    
+ 
+     private void Update()
+     {
+         // get relative range +/-
+         float relRange = (maxAngle - minAngle) / 2f;
+ 
+         // calculate offset
+         float offset = maxAngle - relRange;
+ 
+         // convert to a relative value
+         Vector3 angles = turretToClamp.turret.transform.eulerAngles;
+         float z = ((angles.z + 540) % 360) - 180 - offset;
+ 
+         // if outside range
+         if (Mathf.Abs(z) > relRange)
+         {
+             angles.z = relRange * Mathf.Sign(z) + offset;
+             turretToClamp.turret.transform.eulerAngles = angles;
+         }
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
