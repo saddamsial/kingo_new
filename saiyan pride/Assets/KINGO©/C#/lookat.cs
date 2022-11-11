587a520ebcf19554c5b72eb2b7f022d1a521d261
superior <superior.ppro@gmail.com>
2022-11-11 09:29:42 -0500

    TRYINMG COMBO 5

--- saiyan pride/Assets/KINGO©/C#/lookat.cs
+++ saiyan pride/Assets/KINGO©/C#/lookat.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class lookat : MonoBehaviour
 {
     public float speed = 100f;
+    public Transform Point;
     public Transform target;
+
+    public Quaternion Offset;
     private void LateUpdate()
     {
-        Vector2 direction = target.position - transform.position;
+        Vector2 direction = target.position - Point.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
