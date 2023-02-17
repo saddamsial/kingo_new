990fedf1bd0a2d39f5f000a5fb9c77d36afb83c6
me <82778809+kingo-debug@users.noreply.github.com>
2023-02-17 18:58:02 -0500

    HOME

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/AnimatorFloat.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/AnimatorFloat.cs
@@ -16,10 +16,10 @@ public class AnimatorFloat : MonoBehaviour
     {
 
 
-GameObject childObject = this.transform.GetChild(0).gameObject;
 
 
- animator = childObject.GetComponentInChildren<Animator>();
+
+ animator = this.GetComponent<Animator>();
 
 
 
