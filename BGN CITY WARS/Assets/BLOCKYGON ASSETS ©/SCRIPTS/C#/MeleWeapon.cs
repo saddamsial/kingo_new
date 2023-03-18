a7770b9434dd5716ae19d22d4b264370e32f4ba7
superior <superior.ppro@gmail.com>
2023-03-18 02:29:33 -0400

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/MeleWeapon.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/MeleWeapon.cs
@@ -10,28 +10,38 @@ public WeaponDATA WeaponType;
 public LayerMask layerMask;
 public float Speed;
 public float SwingSize;
+public float Reach;
 public int Swings = 0;
 public bool Fired;
 private float lastshot = 0f;
 public bool Canfire;
 public Transform SwingPoint;
 public AudioSource AS;
-
 private PhotonView PV;
 public PhotonView TPV;
+private RaycastHit hit;
+public Color color;
 
 
 
 
     void Start()
     {
-        
+        PV =  this.GetComponent<PhotonView>();
     }
 
  
     void Update()
     {
   
+          if (Time.time > lastshot + 0.2f)
+        {     
+           
+            Fired = false;     
+   
+           
+        }
+
 
 
    if(Canfire)
@@ -59,10 +69,16 @@ public PhotonView TPV;
      Swings = Swings+1;
      //Reset FireRate
      lastshot = Time.time;
-
+     
+     
        //fire
-     var collider = Physics.OverlapSphere(SwingPoint.position,SwingSize,layerMask);
-     Debug.Log(collider);
+     if (Physics.SphereCast(SwingPoint.position,SwingSize,SwingPoint.forward,out hit,Reach,layerMask))
+     {
+     Debug.Log(hit.collider.name,hit.collider);
+
+     }
+    
+  
 
      }
 
@@ -71,8 +87,12 @@ public PhotonView TPV;
 
 
 
-
-
+        void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(SwingPoint.position,SwingSize);
+    }
+}
 
 
 
@@ -119,4 +139,4 @@ public PhotonView TPV;
 
 
 
-}
+
