69724978a37e933d2bf681c38d696d9a0690f030
me <82778809+kingo-debug@users.noreply.github.com>
2023-03-29 16:26:04 -0400

    CHAT GPT
    ChatGPT <3 helped with rocket launcher and grenade throw

---
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/RocketLauncher.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public Transform launchPoint;
+    public float launchForce = 500f;
+    public float fireRate = 1f;
+    public Transform crosshair;
+    public Transform Player;
+    private float nextFireTime = 0f;
+    public bool canfire;
+
+
+void Start() 
+{
+    Player= transform.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent;
+   
+}
+
+
+
+
+
+
+
+    void Update()
+    {
+         canfire = Player.GetComponent<PlayerActionsVar>().canfire;
+        if (Input.GetButton("Fire1") &&canfire && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + 1f / fireRate;
+            FireRocket();
+        }
+    }
+
+    void FireRocket()
+    {
+        GameObject rocketInstance = Instantiate(rocketPrefab, launchPoint.position, launchPoint.rotation);
+        Rigidbody rocketRigidbody = rocketInstance.GetComponent<Rigidbody>();
+        Vector3 launchDirection = (crosshair.position - launchPoint.position).normalized;
+        rocketRigidbody.AddForce(launchDirection * launchForce);
+    }
+}
