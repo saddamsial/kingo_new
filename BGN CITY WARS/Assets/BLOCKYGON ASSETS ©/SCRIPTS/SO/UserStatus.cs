531d93df5f5ece269742bd6cc2e69d619f2d0a8c
superior <superior.ppro@gmail.com>
2023-07-11 00:13:41 -0400

    SAVE

---
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/SO/UserStatus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New User Data", menuName = "User Data")]
+public class UserStatus : ScriptableObject
+{
+    public string UserName;
+    public int PlayerLevel;
+    public float TimePLayed;
+    public float XP;
+    public int UserID;
+    public int BGNCoins;
+    public Material Skin;
+
+
+
+
+
+
+
+
+}
+
