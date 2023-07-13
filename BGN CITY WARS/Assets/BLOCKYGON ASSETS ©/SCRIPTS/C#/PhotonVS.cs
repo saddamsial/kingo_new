f56cbe8c0d450156b4a809c06ab12bc1b9e32a5c
superior <superior.ppro@gmail.com>
2023-07-13 18:21:19 -0400

    SAVE

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/PhotonVS.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/PhotonVS.cs
@@ -12,9 +12,17 @@ public class PhotonVS : MonoBehaviourPunCallbacks
    public override void OnConnectedToMaster()
     {
         CustomEvent.Trigger(gameObject, "OnConnectedToMaster");
+
     }
     public override void OnJoinedRoom()
     {
         CustomEvent.Trigger(gameObject, "OnJoinedRoom");
     }
+public void SetUserName(string UserName)
+{
+PhotonNetwork.NickName = UserName;
 }
+
+    
+}
+
