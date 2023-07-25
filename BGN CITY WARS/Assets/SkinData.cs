61571e854df321a8c571205e344b629bd1220b14
superior <superior.ppro@gmail.com>
2023-07-25 18:28:22 -0400

    save

---
+++ BGN CITY WARS/Assets/SkinData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinData : MonoBehaviour
+{
+public string SkinName;
+public Material SkinMaterial;
+public Sprite SkinPFP;
+public Color SkinRarity;
+}
