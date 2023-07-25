5396d58ac44c2efb0f6bc99de4e365377836826b
superior <superior.ppro@gmail.com>
2023-07-25 18:04:28 -0400

    lot
    save system for weapons
    save system for user name
    inventory equip spawns in game!

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public void SpawnItemFromID(int id, Vector3 position, Quaternion rotation, Transform parent = null, Vector3 scaleTo = default)
+    public GameObject spawnedItem;
+ 
+    public void SpawnItemFromID(int id, Vector3 localPosition, Quaternion localRotation, Transform parent = null, Vector3 scaleTo = default)
     {
         string prefabName = "Item_" + id.ToString(); // Create the prefab name based on the ID.
 
@@ -10,14 +12,18 @@ public class ItemSpawner : MonoBehaviour
 
         if (itemPrefab != null)
         {
-            // Instantiate the prefab at the specified position and rotation.
-            GameObject spawnedItem = Instantiate(itemPrefab, position, rotation);
+            // Instantiate the prefab at the specified local position and rotation relative to the parent.
+            spawnedItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
 
             // Set the parent if provided.
             if (parent != null)
             {
                 spawnedItem.transform.SetParent(parent);
             }
+
+            // Set the local position and rotation.
+            spawnedItem.transform.localPosition = localPosition;
+            spawnedItem.transform.localRotation = localRotation;
 
             // Set the scale if provided.
             if (scaleTo != default)
