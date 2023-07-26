23ade779beb381451a39cb5d036a81e586376fa8
superior <superior.ppro@gmail.com>
2023-07-26 11:25:05 -0400

    fixed instantiate to netwpork

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public GameObject spawnedItem;
- 
     public void SpawnItemFromID(int id, Vector3 localPosition, Quaternion localRotation, Transform parent = null, Vector3 scaleTo = default)
     {
         string prefabName = "Item_" + id.ToString(); // Create the prefab name based on the ID.
 
-        GameObject itemPrefab = Resources.Load<GameObject>("Prefabs/" + prefabName); // Load the prefab from Resources folder.
+        // Load the prefab from the "Resources" folder directly using the prefabName.
+        GameObject itemPrefab = Resources.Load<GameObject>(prefabName);
 
         if (itemPrefab != null)
         {
-            // Instantiate the prefab at the specified local position and rotation relative to the parent.
-            spawnedItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            // Instantiate the prefab over the network at the specified local position and rotation relative to the parent.
+            GameObject spawnedItem = PhotonNetwork.Instantiate(itemPrefab.name, Vector3.zero, Quaternion.identity);
 
             // Set the parent if provided.
             if (parent != null)
@@ -33,9 +33,7 @@ public class ItemSpawner : MonoBehaviour
         }
         else
         {
-            Debug.LogError("Prefab with name " + prefabName + " not found!");
+            Debug.LogError("Prefab with name " + prefabName + " not found in the 'Resources' folder!");
         }
     }
- 
-  
 }
