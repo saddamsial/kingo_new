76558a31e184deed5dcc43fa261c32f812fb1fa6
superior <superior.ppro@gmail.com>
2023-08-14 11:29:54 -0400

    SAVE

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ItemSpawner.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public void SpawnItemFromID(int id, Vector3 localPosition, Quaternion localRotation, Transform parent = null, Vector3 scaleTo = default)
+    public void SpawnItemFromID(int id, Vector3 localPosition, Quaternion localRotation, string itemPath, Transform parent = null, Vector3 scaleTo = default)
     {
         string prefabName = "Item_" + id.ToString(); // Create the prefab name based on the ID.
 
-        // Load the prefab from the "Resources" folder directly using the prefabName.
-        GameObject itemPrefab = Resources.Load<GameObject>(prefabName);
+        // Combine the itemPath and prefabName using Path.Combine
+        string concatenatedPath = Path.Combine(itemPath,prefabName);
+
+        // Load the prefab from the "Resources" folder using the concatenated path.
+        GameObject spawnempty = Resources.Load<GameObject>("spawnempty");
+        GameObject itemPrefab = Resources.Load<GameObject>(concatenatedPath);
 
         if (itemPrefab != null)
-        {
+        { 
+         
             // Instantiate the prefab over the network at the specified local position and rotation relative to the parent.
-            GameObject spawnedItem = PhotonNetwork.Instantiate(itemPrefab.name, Vector3.zero, Quaternion.identity);
+            GameObject spawnedItem = PhotonNetwork.Instantiate(itemPrefab.name, localPosition, localRotation);
 
             // Set the parent if provided.
             if (parent != null)
