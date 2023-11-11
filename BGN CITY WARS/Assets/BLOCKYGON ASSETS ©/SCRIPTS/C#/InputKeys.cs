7a5eeddc7beee736e2843672841fd7b643378c98
superior <superior.ppro@gmail.com>
2023-11-10 21:22:16 -0500

    save

---
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/InputKeys.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputKeys : MonoBehaviour
+{
+    public bool FireInput;
+    public bool AimInput;
+    public bool PauseInput;
+    public bool JumpInput;
+    public bool ReloadInput;
+   
+
+}
