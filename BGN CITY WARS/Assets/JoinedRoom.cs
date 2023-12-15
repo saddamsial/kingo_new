c7c3ee0d2da5f26145be5bb50cbc1d6bd1001148
superior <superior.ppro@gmail.com>
2023-12-15 16:01:09 -0500

    h

--- BGN CITY WARS/Assets/JoinedRoom.cs
+++ BGN CITY WARS/Assets/JoinedRoom.cs
@@ -8,8 +8,17 @@ public class JoinedRoom : MonoBehaviourPunCallbacks
 {
     public string RoomSceneName;
     public Transform LoadingScreen;
+    [SerializeField]
+    private int MaxroomsAllowed;
+    [SerializeField]
+    private byte MaxPlayersinRoom;
+    [SerializeField]
+    private Transform ErrorMaxRooms;
 
-   
+    [SerializeField]
+    private Transform ErrorRoomFull;
+
+
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel(RoomSceneName);
@@ -23,8 +32,17 @@ public class JoinedRoom : MonoBehaviourPunCallbacks
         pv.RPC("newPlayerJoined", RpcTarget.All);
         
     }
-    public void LoadRoom()
+    public void CreateOrJoinroom()
     {
+        if (PhotonNetwork.CountOfRooms < MaxroomsAllowed)
+        {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = MaxPlayersinRoom; // Set the maximum number of players for the room
 
+            PhotonNetwork.JoinOrCreateRoom("RoomSceneName", roomOptions, TypedLobby.Default);
+
+        }
+        else
+            ErrorMaxRooms.gameObject.SetActive(true);
     }
 }
