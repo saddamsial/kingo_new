91e6d8f7c49b54d65b383a61897bd952741e8ec5
superior <superior.ppro@gmail.com>
2024-01-16 19:32:29 -0500

    Update InitiateData.cs

--- BGN CITY WARS/Assets/InitiateData.cs
+++ BGN CITY WARS/Assets/InitiateData.cs
@@ -15,25 +15,29 @@ public class InitiateData : MonoBehaviour
     }
     void Start()
     {
+        PlayerPrefs.SetInt("NewPlayer", PlayerPrefs.GetInt("NewPlayer"));
         // data initiatation.
-     if ( PlayerPrefs.GetInt (" NewPlayer") ==0 )
+      if (PlayerPrefs.GetInt ("FirstTime") ==0)
         {
             int RandomNums = Random.Range(1000,10000);
 
             //set all initiatated data.
-            PlayerPrefs.SetString("PlayerName", PlayerName + RandomNums.ToString());
+            PlayerPrefs.SetString("PlayerName", "Player" + RandomNums.ToString());
 
             PlayerPrefs.SetInt("BCoins", DefaultCoins);
 
             PlayerPrefs.SetInt("LVL", 1);
 
             PlayerPrefs.SetInt("XP", 0);
+
+            PlayerPrefs.SetInt("FirstTime", 1);
         }
      else
      // data retrieving.
         {
-            PlayerPrefs.GetString("PlayerName", PlayerName);
-
+            //  PlayerPrefs.GetString("PlayerName", PlayerName);
+            int RandomNums = Random.Range(1000, 10000);
+            PlayerPrefs.SetString("PlayerName", PlayerName + RandomNums.ToString());
             PlayerPrefs.SetInt("BCoins", PlayerPrefs.GetInt("BCoins"));
 
             PlayerPrefs.SetInt("LVL", PlayerPrefs.GetInt("LVL"));
@@ -42,5 +46,11 @@ public class InitiateData : MonoBehaviour
         }
     }
 
+    public void PrintTest()
+    {
+        Debug.Log("Printed UserStats_PlayerName " + PlayerPrefs.GetString("UserStats_PlayerName"));
+        Debug.Log("Printed LVL " + PlayerPrefs.GetInt("LVL"));
+        Debug.Log("Printed FirstTime " + PlayerPrefs.GetInt("FirstTime"));
+    }
 
 }
