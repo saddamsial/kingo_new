105abb878bb074f6d17ae3885eaf98bea382fb4d
superior <superior.ppro@gmail.com>
2024-01-23 18:21:18 -0500

    save

--- BGN CITY WARS/Assets/UIBarRefresh.cs
+++ BGN CITY WARS/Assets/UIBarRefresh.cs
@@ -8,18 +8,19 @@ public class UIBarRefresh : MonoBehaviour
     [SerializeField]
     private TMPro.TextMeshProUGUI txt;
     [SerializeField]
-    private Slider fill;
+    private Slider slider;
     private int Hp;
 
     private void Start()
     {
+        Hp = 100;
         UpdateHP(Hp);
     }
     public void UpdateHP(int HP)
     {
         Hp = HP;
         txt.text = HP.ToString() + ("/100");
-        fill.value = HP;
+        slider.value = HP;
 
 
     }
